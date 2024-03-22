5b1832a0c8b995a2002f3aeace48d969e19d1ec8
PreetamGKatakali <preetam19cs051@gmail.com>
2024-03-22 21:06:03 +0530

    Day40 added

---
+++ Day40/JavaScript/index.js
@@ -0,0 +1,21 @@
+var number = Math.random();
+var result;
+if(number <= 0.5)
+{
+  result = 1;
+}
+else{
+   result = 0;
+}
+
+
+var min = 50;
+var max = 100;
+
+var result1 = Math.random()*(max - min)+min;
+var result2 = Math.random()*(max - min)+min;
+var result3 = Math.random()*(max - min)+min;
+
+//result1 = 64.41960433466932
+//result2 = 91.59727835073954
+//result3 = 76.15457234855353
