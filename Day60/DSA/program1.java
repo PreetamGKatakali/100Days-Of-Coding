d2cc66a00bc216b0a4911959f8c629d875c2dbc9
PreetamGKatakali <preetam19cs051@gmail.com>
2024-04-11 19:37:35 +0530

    Day60 added

---
+++ Day60/DSA/program1.java
@@ -0,0 +1,37 @@
+public class program1 {
+    public static void main(String[] args) {
+        int n=6;
+        int i, j;
+        // outer loop to handle upper part
+        for (i = 1; i <= n; i++) {
+            // inner loop to print spaces
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop to print stars
+            for (j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2*i-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+ 
+        // outer loop to handle lower part
+        for (i = n-1; i >= 1; i--) {
+            // inner loop to print spaces
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop to print stars
+            for (j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2*i-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
