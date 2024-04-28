215506c62fb04b4e5cf09d697f99b9de59ff23f0
PreetamGKatakali <preetam19cs051@gmail.com>
2024-04-28 18:57:11 +0530

    Day77 added

--- Day75/DSA/program.java
+++ Day75/DSA/program.java
@@ -1,5 +1,66 @@
 package Day75.DSA;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/** BFS in the binary tree is nothing but the level order tree travesal */
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
 public class program {
-    
+    static int index=-1;
+    static Node binaryTree(int[] nodes){
+        ++index;
+        if(nodes[index]==-1){
+            return null;
+        }
+        Node newNode=new Node(nodes[index]);
+        newNode.left=binaryTree(nodes);
+        newNode.right=binaryTree(nodes);
+        return newNode;
+    }
+    static void BFS(Node root){
+        if(root==null){
+            return;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node currentNode=q.remove();
+            if(currentNode==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                System.out.print(currentNode.val+" ");
+                if(currentNode.left!=null){
+                    q.add(currentNode.left);
+                }
+                if(currentNode.right!=null){
+                    q.add(currentNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root=binaryTree(nodes);
+
+        BFS(root);
+
+    }
 }
