c95fe503a1db27ae23c27153409cdf8e4d1cea23
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-08 18:41:51 +0530

    Day87 added

---
+++ Day87/DSA/program.java
@@ -0,0 +1,66 @@
+package Day87.DSA;
+
+import java.util.ArrayList;
+class Edge{
+    int src;
+    int des;
+    Edge(int src,int des){
+        this.src=src;
+        this.des=des;
+    }
+}
+public class program {
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+    }
+    public static void allPaths(ArrayList<Edge> graphs[],boolean vis[],int curr,String path,int target){
+        if(curr==target){
+            System.out.println(path);
+            return ;
+        }
+        for(int i=0;i<graphs[curr].size();i++){
+            Edge e=graphs[curr].get(i);
+            if(!vis[e.des]){
+                vis[curr]=true;
+                allPaths(graphs, vis,e.des, path+e.des, target);
+                vis[curr]=false;
+            }
+        }
+    }
+    public static void main(String[] args) {
+         int V=7;
+        ArrayList<Edge> graph[]=new ArrayList[V];
+        createGraph(graph);
+
+        boolean[] vis=new boolean[V];
+
+        int start=0,target=5;
+        allPaths(graph, vis, start, "0", target);
+    }
+    
+}
