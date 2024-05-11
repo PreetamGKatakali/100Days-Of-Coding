cf0d48d113d1d5299c7dc3c8cee0a83aa0ecef1d
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-11 13:14:41 +0530

    Day90 added

--- Day89/DSA/program1.java
+++ Day89/DSA/program1.java
@@ -1,7 +1,50 @@
 package Day89.DSA;
-
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
 public class program1 {
+    static Node root;
+
+    static void insert(int[] nodes){
+        for(int i=0;i<nodes.length;i++){
+            root=binaryTree(root,nodes[i]);
+        }
+    }
+    static Node binaryTree(Node root,int val){
+        if(root==null){
+            root=new Node(val);
+            return root;
+        }
+        if(val%2==0){
+            root.left=binaryTree(root.left, val);
+        }
+        else{
+            root.right=binaryTree(root.right, val);
+        }
+        return root;
+    }
+
+    static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.val+" ");
+        inorder(root.right);
+    }
+
     public static void main(String[] args) {
-        
+        int[] nodes={5,8,3,2,7};
+
+        insert(nodes);
+
+        inorder(root);
     }
 }
