7707f994d388b9f028581856aa4987358abccf75
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-18 11:37:32 +0530

    Day97 added

--- Day96/DSA/program1.java
+++ Day96/DSA/program1.java
@@ -1,5 +1,27 @@
+import javax.swing.tree.TreeNode;
+
 public class program1 {
+    int count =0;
+    int out=0;
+    public int kthSmallest(TreeNode root, int k) {
+        recur(root, k);
+        return out;
+    }
+    
+    public void recur(TreeNode node, int k){
+        if(node.left != null){
+            recur(node.left, k);
+        }
+        count++;
+        if(count == k){
+            out = node.val;
+            return;
+        }
+        if(node.right != null){
+            recur(node.right, k);
+        }
+    }
     public static void main(String[] args) {
-        
+        //Node in the BST
     }
 }
