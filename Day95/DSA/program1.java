8f021f034e5182f58105e491322d73681b69eeca
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-18 11:36:51 +0530

    Day97 added

--- Day95/DSA/program1.java
+++ Day95/DSA/program1.java
@@ -1,5 +1,40 @@
+import javax.swing.tree.TreeNode;
+
 public class program1 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        //search
+        if(root == null ) {
+            return null;
+        }
+        if(root.val<key){
+            root.right = deleteNode(root.right,key);
+        }
+        else if(root.val>key) {
+            root.left = deleteNode(root.left,key);
+        }
+        else{
+            //case 1: leaf node
+            if(root.left == null && root.right == null ) {
+                return null;
+            }
+            //case 2 : one child
+            if(root.left ==  null){
+                return root.right;
+            }
+            else if(root.right ==  null ) {
+                return root.left;
+            }
+            //case 3 : two children
+            TreeNode IS = findInOrderSuccessor(root.right);
+            root.val = IS.val;
+
+            
+            root.right = deleteNode(root.right,IS.val);
+        }
+        return root;
+
+    }
     public static void main(String[] args) {
-        
+        //Node in the Binary Tree
     }
 }
