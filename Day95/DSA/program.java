9785b8386c78ed15878c24bb778be8f6e92f15e2
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-18 11:36:46 +0530

    Day97 added

--- Day95/DSA/program.java
+++ Day95/DSA/program.java
@@ -1,5 +1,27 @@
 public class program{
+    public Node  insertIntoBst(Node root,int val){
+        if(root==null) return new Node(val);
+        Node cur=root;
+        while(true){
+            if(cur.val<=val){
+                if(cur.right!=null) cur=cur.right;
+                else{
+                    cur.right=new Node(val);
+                    break;
+                }
+            }
+            else{
+                if(cur.left!=null) cur=cur.left;
+                else{
+                    cur.left=new Node(val);
+                    break;
+                }
+            }
+        }
+
+        return root;
+    }
     public static void main(String[] args) {
-        
+        //Node in the Binary tree
     }
 }
