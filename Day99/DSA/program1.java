e543bc05c20a844cd6c10e6426edb4f2b479c15f
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-21 19:05:58 +0530

    Day100 added

--- Day99/DSA/program1.java
+++ Day99/DSA/program1.java
@@ -1,7 +1,23 @@
 package Day99.DSA;
 
 public class program1 {
+    static public int rob(int[] nums) {
+        int costPrevToPrevHouse = nums[0];
+
+        if (nums.length == 1) {
+            return costPrevToPrevHouse;
+        }
+        int costPrevHouse = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < nums.length; i++) {
+            int costCurrentHouse = Math.max(costPrevHouse, costPrevToPrevHouse + nums[i]);
+            costPrevToPrevHouse = costPrevHouse;
+            costPrevHouse = costCurrentHouse;
+        }
+        return costPrevHouse;
+    }
     public static void main(String[] args) {
-        
+        int[] arr={1,2,3,1};
+        System.out.println(rob(arr));
     }
 }
