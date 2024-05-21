f187ea645b9b76fcfd45421448b0511ee2262f88
PreetamGKatakali <preetam19cs051@gmail.com>
2024-05-21 19:05:21 +0530

    Day100 added

--- Day76/DSA/program.java
+++ Day76/DSA/program.java
@@ -1,5 +1,16 @@
 package Day76.DSA;
 
 public class program {
-    
+    static public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 31; i++) {
+            int ans = (int) Math.pow(2, i);
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+    }
 }
